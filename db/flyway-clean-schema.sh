e34a3d111cbfc573ca030a65d3208a8256438f74
Jesus Carpintero <jesus.carpintero@adevinta.com>
2021-10-20 10:16:33 +0200

    Upgrade flyway image

--- db/flyway-clean-schema.sh
+++ db/flyway-clean-schema.sh
@@ -2,4 +2,5 @@
 
 # Copyright 2021 Adevinta
 
-docker run --net="host" -v $(pwd):/scripts boxfuse/flyway -user=vulcan_reportgen -password=vulcan_reportgen -url=jdbc:postgresql://localhost:5438/vulcan_reportgen -baselineOnMigrate=true -locations=filesystem:/scripts/ clean
+docker run --net="host" -v $(pwd):/flyway/sql flyway/flyway:8-alpine \
+    -user=vulcan_reportgen -password=vulcan_reportgen -url=jdbc:postgresql://localhost:5438/vulcan_reportgen -baselineOnMigrate=true clean
