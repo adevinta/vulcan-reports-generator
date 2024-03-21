d347b79ed0e9cfa7567599586d1863e681e439dc
Jesús Carpintero <jesus.carpintero@gmail.com>
2024-03-21 10:22:05 +0100

    Set fallback version to 10

--- db/flyway-clean-schema.sh
+++ db/flyway-clean-schema.sh
@@ -2,5 +2,5 @@
 
 # Copyright 2021 Adevinta
 
-docker run --net=host -v "$PWD":/flyway/sql flyway/flyway:"${FLYWAY_VERSION:-9}-alpine" \
+docker run --net=host -v "$PWD":/flyway/sql flyway/flyway:"${FLYWAY_VERSION:-10}-alpine" \
     -user=vulcan_reportgen -password=vulcan_reportgen -url=jdbc:postgresql://localhost:5438/vulcan_reportgen -baselineOnMigrate=true -cleanDisabled=false clean
